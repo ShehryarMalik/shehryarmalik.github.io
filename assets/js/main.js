a3b13ab13d58cee9bdf5505286e05e2f91a737bb
ShehryarMalik <shehryar.malik07@gmail.com>
2022-08-18 18:46:35 +0500

    -Added Loading screen -Added Responsive design for images -Added Responsive design for youtube embed videos

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,3 @@
-/*
-	Dimension by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
-
 (function($) {
 
 	var	$window = $(window),
@@ -27,6 +21,7 @@
 	// Play initial animations on page load.
 		$window.on('load', function() {
 			window.setTimeout(function() {
+				$(".se-pre-con").fadeOut("slow");
 				$body.removeClass('is-preload');
 			}, 100);
 		});
